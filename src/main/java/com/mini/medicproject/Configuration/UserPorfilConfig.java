c13670c689cb4fa5f09344f5c059541d675c37cb
jogisiagian <39945511+jogisiagian@users.noreply.github.com>
2025-02-11 17:37:17 +0700

    test

--- src/main/java/com/mini/medicproject/Configuration/UserPorfilConfig.java
+++ src/main/java/com/mini/medicproject/Configuration/UserPorfilConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class UserPorfilConfig implements WebMvcConfigurer {
 
-	String mypath = "file:///D:\\Foto\\javafile\\";
+	String mypath = "file:///D:\\Foto\\javafile\\test\\";
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
